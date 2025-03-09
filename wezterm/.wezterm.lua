320accecd0e5ab6947fad660cdb2de365d37df0b
raphael-fournier <raphaelfournier.pro@gmail.com>
2025-03-09 17:02:45 +0100

    feat(wezterm): format config file

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -33,12 +33,32 @@ config.keys = {
   { key = "l", mods = "CMD", action = act.ActivatePaneDirection("Right") },
   { key = "k", mods = "CMD", action = act.ActivatePaneDirection("Up") },
   { key = "j", mods = "CMD", action = act.ActivatePaneDirection("Down") },
-  { key = "w", mods = "CMD", action = act.CloseCurrentPane({ confirm = false }) },
+  {
+    key = "w",
+    mods = "CMD",
+    action = act.CloseCurrentPane({ confirm = false }),
+  },
   { key = "`", mods = "CMD", action = act.SwitchWorkspaceRelative(1) },
-  { key = "LeftArrow", mods = "CMD", action = wezterm.action.AdjustPaneSize({ "Left", 5 }) },
-  { key = "RightArrow", mods = "CMD", action = wezterm.action.AdjustPaneSize({ "Right", 5 }) },
-  { key = "DownArrow", mods = "CMD", action = wezterm.action.AdjustPaneSize({ "Down", 5 }) },
-  { key = "UpArrow", mods = "CMD", action = wezterm.action.AdjustPaneSize({ "Up", 5 }) },
+  {
+    key = "LeftArrow",
+    mods = "CMD",
+    action = wezterm.action.AdjustPaneSize({ "Left", 5 }),
+  },
+  {
+    key = "RightArrow",
+    mods = "CMD",
+    action = wezterm.action.AdjustPaneSize({ "Right", 5 }),
+  },
+  {
+    key = "DownArrow",
+    mods = "CMD",
+    action = wezterm.action.AdjustPaneSize({ "Down", 5 }),
+  },
+  {
+    key = "UpArrow",
+    mods = "CMD",
+    action = wezterm.action.AdjustPaneSize({ "Up", 5 }),
+  },
 
   -- ZenMaid workspace launch
   {
@@ -53,26 +73,43 @@ wezterm.on("launch_zenmaid_workspace", function()
   local project_dir = wezterm.home_dir .. "/Workspace/pro/zenmaid"
 
   -- Set up tabs and panes for ZenMaid webapp
-  local webapp_tab, webapp_pane_1, zenmaid_window = mux.spawn_window({
+  local _, webapp_pane_1, zenmaid_window = mux.spawn_window({
     workspace = "zenmaid",
     cwd = project_dir .. "/zenmaid-webapp",
   })
-  local webapp_pane_2 = webapp_pane_1:split({ direction = "Right", cwd = project_dir .. "/zenmaid-webapp/frontend" })
-  local webapp_pane_3 = webapp_pane_1:split({ direction = "Bottom", cwd = project_dir .. "/zenmaid-webapp" })
-  local webapp_pane_4 = webapp_pane_2:split({ direction = "Bottom", cwd = project_dir .. "/zenmaid-webapp" })
+  local webapp_pane_2 = webapp_pane_1:split({
+    direction = "Right",
+    cwd = project_dir .. "/zenmaid-webapp/frontend",
+  })
+  local webapp_pane_3 = webapp_pane_1:split({
+    direction = "Bottom",
+    cwd = project_dir .. "/zenmaid-webapp",
+  })
+  local webapp_pane_4 = webapp_pane_2:split({
+    direction = "Bottom",
+    cwd = project_dir .. "/zenmaid-webapp",
+  })
 
-  webapp_pane_1:send_text("OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES bundle exec rails s\n")
+  webapp_pane_1:send_text(
+    "OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES bundle exec rails s\n"
+  )
   webapp_pane_2:send_text("yarn dev\n")
   webapp_pane_3:send_text("bundle exec rails c\n")
   webapp_pane_4:send_text("bundle exec sidekiq\n")
 
   -- Set up tab for ZenMaid mobile project
-  local mobile_tab, mobile_pane_1 = zenmaid_window:spawn_tab({ cwd = project_dir .. "/zenmaid-mobile" })
-  local mobile_pane_2 = mobile_pane_1:split({ direction = "Right", cwd = project_dir .. "/zenmaid-mobile" })
+  local _, mobile_pane_1 =
+    zenmaid_window:spawn_tab({ cwd = project_dir .. "/zenmaid-mobile" })
+  local _ = mobile_pane_1:split({
+    direction = "Right",
+    cwd = project_dir .. "/zenmaid-mobile",
+  })
   mobile_pane_1:send_text("yarn run start:local\n")
 
   mux.set_active_workspace("zenmaid")
-  zenmaid_window:gui_window():perform_action(wezterm.action.ActivateTab(0), webapp_pane_1)
+  zenmaid_window
+    :gui_window()
+    :perform_action(wezterm.action.ActivateTab(0), webapp_pane_1)
   zenmaid_window:gui_window():maximize()
 end)
 
@@ -87,7 +124,8 @@ end)
 
 -- GUI Startup Settings
 wezterm.on("gui-startup", function()
-  local _, _, default_window = wezterm.mux.spawn_window({ workspace = "default" })
+  local _, _, default_window =
+    wezterm.mux.spawn_window({ workspace = "default" })
   wezterm.mux.set_active_workspace("default")
   default_window:gui_window():maximize()
 end)

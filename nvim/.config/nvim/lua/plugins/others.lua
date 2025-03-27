beeb75c57b005ac7913f6b49e05583a75c07f8b8
raphael-fournier <raphaelfournier.pro@gmail.com>
2025-03-27 23:05:34 +0100

    nvim: add minor plugins

--- nvim/.config/nvim/lua/plugins/others.lua
+++ nvim/.config/nvim/lua/plugins/others.lua
@@ -1,6 +1,15 @@
 return {
   "tpope/vim-sleuth", -- Detect tabstop and shiftwidth automatically
   "eandrju/cellular-automaton.nvim", -- Cool animations
+  { "echasnovski/mini.pairs", version = "*" },
+  { "echasnovski/mini.surround", version = "*" },
+  {
+    "windwp/nvim-autopairs",
+    event = "InsertEnter",
+    config = true,
+    -- use opts = {} for passing setup options
+    -- this is equivalent to setup({}) function
+  },
   { -- Scroll after the end of file
     "Aasim-A/scrollEOF.nvim",
     event = { "CursorMoved", "WinScrolled" },
@@ -10,6 +19,29 @@ return {
         -- Whether or not scrollEOF should be enabled in floating windows
         floating = false,
       })
+    end,
+  },
+  {
+    "andrewferrier/debugprint.nvim",
+    -- dependencies = {
+    --   "echasnovski/mini.nvim", -- Needed for :ToggleCommentDebugPrints(NeoVim 0.9 only)
+    --   -- and line highlighting (optional)
+    -- },
+    opts = {
+      keymaps = {
+        visual = {
+          variable_below = "<leader>l",
+        },
+      },
+    },
+    version = "*", -- Remove if you DON'T want to use the stable version
+    -- lazy = false, -- Required to make line highlighting work before debugprint is first used
+  },
+  {
+    "nvim-treesitter/nvim-treesitter-context",
+    opts = {},
+    config = function()
+      vim.api.nvim_set_hl(0, "TreesitterContext", { bg = "None" })
     end,
   },
 }

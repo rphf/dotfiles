c0875e6bd5b3e5baca0ccf8fe71e00322c9dea27
raphael-fournier <raphaelfournier.pro@gmail.com>
2025-03-27 23:14:10 +0100

    wezterm: configure some more keybindings

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,4 +1,4 @@
-local wezterm = require("wezterm")
+local wezterm = require("wezterm") -- --[[@as Wezterm]] TODO: Fix type anotation for weztem (using lazydev)
 local mux = wezterm.mux
 local act = wezterm.action
 
@@ -8,11 +8,13 @@ local config = {}
 config.font = wezterm.font("JetBrainsMonoNL Nerd Font Mono")
 config.font_size = 12.5
 config.color_scheme = "Tokyo Night Storm"
-config.macos_window_background_blur = 15
-config.window_background_opacity = 0.9
+-- config.macos_window_background_blur = 15
+-- config.window_background_opacity = 0.9
 config.inactive_pane_hsb = { brightness = 0.5 }
 config.window_padding = { left = 10, right = 10, top = 10, bottom = 10 }
 config.colors = {
+  -- cursor_bg = "7aa2f7",
+  -- cursor_border = "7aa2f7",
   tab_bar = {
     background = "rgba(0,0,0,0.5)",
   },
@@ -24,10 +26,9 @@ config.use_fancy_tab_bar = false
 -- General Settings
 config.audible_bell = "Disabled"
 
--- stylua: ignore
 -- Key Bindings
 config.keys = {
-  { key = "p", mods = "CMD", action = wezterm.action.ShowLauncher },
+  { key = "p", mods = "CMD", action = act.ShowLauncher },
   { key = "u", mods = "CMD", action = act.SplitVertical },
   { key = "i", mods = "CMD", action = act.SplitHorizontal },
   { key = "h", mods = "CMD", action = act.ActivatePaneDirection("Left") },
@@ -36,13 +37,16 @@ config.keys = {
   { key = "j", mods = "CMD", action = act.ActivatePaneDirection("Down") },
   { key = "w", mods = "CMD", action = act.CloseCurrentPane({ confirm = false }) },
   { key = "`", mods = "CMD", action = act.SwitchWorkspaceRelative(1) },
-  { key = "LeftArrow", mods = "CMD", action = wezterm.action.AdjustPaneSize({ "Left", 5 }) },
-  { key = "RightArrow", mods = "CMD", action = wezterm.action.AdjustPaneSize({ "Right", 5 }) },
-  { key = "DownArrow", mods = "CMD", action = wezterm.action.AdjustPaneSize({ "Down", 5 }) },
-  { key = "UpArrow", mods = "CMD", action = wezterm.action.AdjustPaneSize({ "Up", 5 }) },
-
+  { key = "H", mods = "CMD", action = act.ActivateTabRelative(-1) },
+  { key = "L", mods = "CMD", action = act.ActivateTabRelative(1) },
+  { key = "o", mods = "CMD", action = act.TogglePaneZoomState },
+  -- Rezize panes
+  { key = "LeftArrow", mods = "CMD", action = act.AdjustPaneSize({ "Left", 5 }) },
+  { key = "RightArrow", mods = "CMD", action = act.AdjustPaneSize({ "Right", 5 }) },
+  { key = "DownArrow", mods = "CMD", action = act.AdjustPaneSize({ "Down", 5 }) },
+  { key = "UpArrow", mods = "CMD", action = act.AdjustPaneSize({ "Up", 5 }) },
   -- ZenMaid workspace launch
-  { key = "z", mods = "OPT", action = wezterm.action.EmitEvent("launch_zenmaid_workspace") },
+  { key = "z", mods = "OPT", action = act.EmitEvent("launch_zenmaid_workspace") },
 }
 
 -- Workspace Configuration (ZenMaid Workspace)
@@ -67,16 +71,13 @@ wezterm.on("launch_zenmaid_workspace", function()
     cwd = project_dir .. "/zenmaid-webapp",
   })
 
-  webapp_pane_1:send_text(
-    "OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES bundle exec rails s\n"
-  )
+  webapp_pane_1:send_text("OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES bundle exec rails s\n")
   webapp_pane_2:send_text("yarn dev\n")
   webapp_pane_3:send_text("bundle exec rails c\n")
   webapp_pane_4:send_text("bundle exec sidekiq\n")
 
   -- Set up tab for ZenMaid mobile project
-  local _, mobile_pane_1 =
-    zenmaid_window:spawn_tab({ cwd = project_dir .. "/zenmaid-mobile" })
+  local _, mobile_pane_1 = zenmaid_window:spawn_tab({ cwd = project_dir .. "/zenmaid-mobile" })
   local _ = mobile_pane_1:split({
     direction = "Right",
     cwd = project_dir .. "/zenmaid-mobile",
@@ -84,9 +85,7 @@ wezterm.on("launch_zenmaid_workspace", function()
   mobile_pane_1:send_text("yarn run start:local\n")
 
   mux.set_active_workspace("zenmaid")
-  zenmaid_window
-    :gui_window()
-    :perform_action(wezterm.action.ActivateTab(0), webapp_pane_1)
+  zenmaid_window:gui_window():perform_action(act.ActivateTab(0), webapp_pane_1)
   zenmaid_window:gui_window():maximize()
 end)
 
@@ -101,8 +100,7 @@ end)
 
 -- GUI Startup Settings
 wezterm.on("gui-startup", function()
-  local _, _, default_window =
-    wezterm.mux.spawn_window({ workspace = "default" })
+  local _, _, default_window = wezterm.mux.spawn_window({ workspace = "default" })
   wezterm.mux.set_active_workspace("default")
   default_window:gui_window():maximize()
 end)

6cefed9e83fe5514f4ab0386ef63fdb5740064c5
raphael-fournier <raphaelfournier.pro@gmail.com>
2025-03-27 23:25:36 +0100

    nvim: refine snacks plugin config

--- nvim/.config/nvim/lua/plugins/snacks.lua
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -4,7 +4,6 @@ return {
   lazy = false,
   dependencies = { "echasnovski/mini.icons" },
   opts = {
-    dashboard = { enabled = true, example = "compact_files" },
     notifier = {
       enabled = true,
       timeout = 3000,
@@ -12,6 +11,19 @@ return {
     explorer = { enabled = true, replace_netwr = false },
     picker = {
       enabled = true,
+      layout = {
+        fullscreen = true,
+      },
+      formatters = {
+        file = {
+          truncate = 100, -- truncate the file path to (roughly) this length
+        },
+      },
+      matcher = {
+        -- Search within folder name with the same weight thanfile name
+        -- It helps searching file in codebase with a lot of identical filename such as `index.ts`
+        filename_bonus = false,
+      },
       sources = {
         explorer = {
           auto_close = true,
@@ -20,37 +32,44 @@ return {
             preview = true,
             fullscreen = true,
           },
+          -- layout = {
+          --   layout = {
+          --     width = 0.4,
+          --   },
+          -- },
         },
       },
     },
     indent = {
       animate = { duration = { step = 20, total = 200 } },
     },
+    input = { enabled = true },
+
     -- statuscolumn = { enabled = true },
     -- git = { enabled = true },
     -- bigfile = { enabled = true },
-    -- input = { enabled = true },
     -- quickfile = { enabled = true },
     -- scope = { enabled = true },
     -- scroll = { enabled = true },
     -- words = { enabled = true },
   },
+
   -- stylua: ignore
   -- TODO: Refactor this table to only keep what I actually use
   keys = {
     -- Top Pickers & Explorer
-    { "<leader><space>", function() require("snacks").picker.smart() end, desc = "Smart Find Files" },
-    { "<leader>,", function() require("snacks").picker.buffers() end, desc = "Buffers" },
-    { "<leader>/", function() require("snacks").picker.grep({hidden = true}) end, desc = "Grep" },
+    -- { "<leader>f", function() require("snacks").picker.files( { hidden = true }) end, desc = "find files" },
+    { "<leader>f", function() require("snacks").picker.smart( { filter = { cwd = true }, hidden = true } ) end, desc = "Smart Find Files" },
+    { "<leader>/", function() require("snacks").picker.grep( { hidden = true } ) end, desc = "Grep" },
     { "<leader>:", function() require("snacks").picker.command_history() end, desc = "Command History" },
     { "<leader>n", function() require("snacks").picker.notifications() end, desc = "Notification History" },
     { "<leader>e", function() require("snacks").explorer( { hidden = true } ) end, desc = "File Explorer" },
-    -- find
-    { "<leader>fb", function() require("snacks").picker.buffers(
+    { "<leader>b", function() require("snacks").picker.buffers(
       {
-        on_show = function()
-          vim.cmd.stopinsert()
-        end,
+        -- Open directly in normal mode
+        -- on_show = function()
+        --   vim.cmd.stopinsert()
+        -- end,
         win = {
           input = {
             keys = {
@@ -59,32 +78,22 @@ return {
           },
           list = { keys = { ["d"] = "bufdelete" } },
         },
-        layout = {fullscreen= true},
       }
       ) end, desc = "Buffers" },
-    { "<leader>fc", function() require("snacks").picker.files({ cwd = vim.fn.stdpath("config") }) end, desc = "Find Config File" },
-    { "<leader>ff", function() require("snacks").picker.files() end, desc = "Find Files" },
-    { "<leader>fg", function() require("snacks").picker.git_files() end, desc = "Find Git Files" },
-    { "<leader>fp", function() require("snacks").picker.projects() end, desc = "Projects" },
-    { "<leader>fr", function() require("snacks").picker.recent() end, desc = "Recent" },
+    { "<leader>p", function() vim.cmd("SessionSearch") end, desc = "Projects" },
+    { "<leader>sr", function() require("snacks").picker.recent() end, desc = "Recent" },
     -- git
     { "<leader>gb", function() require("snacks").picker.git_branches() end, desc = "Git Branches" },
     { "<leader>gl", function() require("snacks").picker.git_log() end, desc = "Git Log" },
     { "<leader>gL", function() require("snacks").picker.git_log_line() end, desc = "Git Log Line" },
     { "<leader>gs", function() require("snacks").picker.git_status() end, desc = "Git Status" },
     { "<leader>gS", function() require("snacks").picker.git_stash() end, desc = "Git Stash" },
-    { "<leader>gd", function() require("snacks").picker.git_diff() end, desc = "Git Diff (Hunks)" },
     { "<leader>gf", function() require("snacks").picker.git_log_file() end, desc = "Git Log File" },
-    -- Grep
-    { "<leader>sb", function() require("snacks").picker.lines() end, desc = "Buffer Lines" },
-    { "<leader>sB", function() require("snacks").picker.grep_buffers() end, desc = "Grep Open Buffers" },
-    { "<leader>sg", function() require("snacks").picker.grep() end, desc = "Grep" },
-    { "<leader>sw", function() require("snacks").picker.grep_word() end, desc = "Visual selection or word", mode = { "n", "x" } },
+    { "<leader>gB", function() require("snacks").gitbrowse() end, desc = "Git Browse", mode = { "n", "v" } },
+    { "<leader>gg", function() require("snacks").lazygit() end, desc = "Lazygit" },
     -- search
     { '<leader>s"', function() require("snacks").picker.registers() end, desc = "Registers" },
     { '<leader>s/', function() require("snacks").picker.search_history() end, desc = "Search History" },
-    { "<leader>sa", function() require("snacks").picker.autocmds() end, desc = "Autocmds" },
-    { "<leader>sb", function() require("snacks").picker.lines() end, desc = "Buffer Lines" },
     { "<leader>sc", function() require("snacks").picker.command_history() end, desc = "Command History" },
     { "<leader>sC", function() require("snacks").picker.commands() end, desc = "Commands" },
     { "<leader>sd", function() require("snacks").picker.diagnostics() end, desc = "Diagnostics" },
@@ -97,7 +106,7 @@ return {
     { "<leader>sl", function() require("snacks").picker.loclist() end, desc = "Location List" },
     { "<leader>sm", function() require("snacks").picker.marks() end, desc = "Marks" },
     { "<leader>sM", function() require("snacks").picker.man() end, desc = "Man Pages" },
-    { "<leader>sp", function() require("snacks").picker.lazy() end, desc = "Search for Plugin Spec" },
+    { "<leader>sp", function() require("snacks").picker.lazy() end,desc = "Search for Plugin Spec" },
     { "<leader>sq", function() require("snacks").picker.qflist() end, desc = "Quickfix List" },
     { "<leader>sR", function() require("snacks").picker.resume() end, desc = "Resume" },
     { "<leader>su", function() require("snacks").picker.undo() end, desc = "Undo History" },
@@ -108,41 +117,9 @@ return {
     { "gr", function() require("snacks").picker.lsp_references() end, nowait = true, desc = "References" },
     { "gI", function() require("snacks").picker.lsp_implementations() end, desc = "Goto Implementation" },
     { "gy", function() require("snacks").picker.lsp_type_definitions() end, desc = "Goto T[y]pe Definition" },
-    { "<leader>ss", function() require("snacks").picker.lsp_symbols() end, desc = "LSP Symbols" },
-    { "<leader>sS", function() require("snacks").picker.lsp_workspace_symbols() end, desc = "LSP Workspace Symbols" },
     -- Other
-    { "<leader>z",  function() require("snacks").zen() end, desc = "Toggle Zen Mode" },
-    { "<leader>Z",  function() require("snacks").zen.zoom() end, desc = "Toggle Zoom" },
-    { "<leader>.",  function() require("snacks").scratch() end, desc = "Toggle Scratch Buffer" },
-    { "<leader>S",  function() require("snacks").scratch.select() end, desc = "Select Scratch Buffer" },
-    { "<leader>n",  function() require("snacks").notifier.show_history() end, desc = "Notification History" },
-    { "<leader>bd", function() require("snacks").bufdelete() end, desc = "Delete Buffer" },
-    { "<leader>cR", function() require("snacks").rename.rename_file() end, desc = "Rename File" },
-    { "<leader>gB", function() require("snacks").gitbrowse() end, desc = "Git Browse", mode = { "n", "v" } },
-    { "<leader>gg", function() require("snacks").lazygit() end, desc = "Lazygit" },
+    { "<leader>d", function() require("snacks").bufdelete() end, desc = "Delete Buffer" },
     { "<leader>un", function() require("snacks").notifier.hide() end, desc = "Dismiss All Notifications" },
-    { "<c-/>",      function() require("snacks").terminal() end, desc = "Toggle Terminal" },
-    { "<c-_>",      function() require("snacks").terminal() end, desc = "which_key_ignore" },
-    { "]]",         function() require("snacks").words.jump(vim.v.count1) end, desc = "Next Reference", mode = { "n", "t" } },
-    { "[[",         function() require("snacks").words.jump(-vim.v.count1) end, desc = "Prev Reference", mode = { "n", "t" } },
-    {
-      "<leader>N",
-      desc = "Neovim News",
-      function()
-        require("snacks").win({
-          file = vim.api.nvim_get_runtime_file("doc/news.txt", false)[1],
-          width = 0.6,
-          height = 0.6,
-          wo = {
-            spell = false,
-            wrap = false,
-            signcolumn = "yes",
-            statuscolumn = " ",
-            conceallevel = 3,
-          },
-        })
-      end,
-    }
   },
   init = function()
     vim.api.nvim_create_autocmd("User", {
@@ -157,7 +134,6 @@ return {
         end
         vim.print = _G.dd -- Override print to use snacks for `:=` command
 
-        -- stylua: ignore start
         -- Create some toggle mappings
         require("snacks").toggle.option("spell", { name = "Spelling" }):map("<leader>us")
         require("snacks").toggle.option("wrap", { name = "Wrap" }):map("<leader>uw")
@@ -174,7 +150,6 @@ return {
         require("snacks").toggle.inlay_hints():map("<leader>uh")
         require("snacks").toggle.indent():map("<leader>ug")
         require("snacks").toggle.dim():map("<leader>uD")
-        -- stylua: ignore end
       end,
     })
   end,

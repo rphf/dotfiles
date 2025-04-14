955108d21bb706bfc0558d10bea360ddec787bfe
raphael-fournier <93053988+raphael-fournier@users.noreply.github.com>
2025-04-14 02:17:50 +0200

    nvim: update grug-far config

--- nvim/.config/nvim/lua/plugins/find-and-replace.lua
+++ nvim/.config/nvim/lua/plugins/find-and-replace.lua
@@ -9,6 +9,15 @@ return {
       -- engine = 'ripgrep' is default, but 'astgrep' or 'astgrep-rules' can
       -- be specified
     })
+
+    -- Get the Search highlight group
+    local search_hl = vim.api.nvim_get_hl(0, { name = "Search" })
+
+    -- Set GrugFarResultsMatch with only the needed fields
+    vim.api.nvim_set_hl(0, "GrugFarResultsMatch", {
+      fg = search_hl.fg,
+      bg = search_hl.bg,
+    })
   end,
   keys = {
     {
@@ -21,6 +30,23 @@ return {
           prefills = {
             filesFilter = ext and ext ~= "" and "*." .. ext or nil,
             flags = "--fixed-strings --hidden -i",
+          },
+        })
+      end,
+      mode = { "n", "v" },
+      desc = "Search and Replace",
+    },
+    {
+      "<leader>sR",
+      function()
+        local grug = require("grug-far")
+        local ext = vim.bo.buftype == "" and vim.fn.expand("%:e")
+        grug.open({
+          transient = true,
+          prefills = {
+            filesFilter = ext and ext ~= "" and "*." .. ext or nil,
+            flags = "--fixed-strings --hidden -i",
+            paths = vim.fn.expand("%"),
           },
         })
       end,

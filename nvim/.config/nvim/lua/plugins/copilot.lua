fa0af3f090af7471fa526de764cd80adcec29790
raphael-fournier <93053988+raphael-fournier@users.noreply.github.com>
2025-05-01 22:13:07 +0200

    nvim: add copilot plugin

---
+++ nvim/.config/nvim/lua/plugins/copilot.lua
@@ -0,0 +1,24 @@
+return {
+  "zbirenbaum/copilot.lua",
+  cmd = "Copilot",
+  build = ":Copilot auth",
+  event = "BufReadPost",
+  opts = {
+    suggestion = {
+      enabled = true,
+      auto_refresh = true,
+      auto_trigger = true,
+      hide_during_completion = false,
+      keymap = {
+        accept = "<Tab>",
+        next = "<M-]>",
+        prev = "<M-[>",
+      },
+    },
+    panel = { enabled = false },
+    filetypes = {
+      markdown = true,
+      help = true,
+    },
+  },
+}
